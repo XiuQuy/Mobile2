a6fdb4d0a7e0e0b977595229bafa2961d468eac7
anhvo506 <85537815+anhvo506@users.noreply.github.com>
2024-03-27 09:36:45 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/appxemphim/VideoResponse.java
@@ -0,0 +1,13 @@
+package com.example.appxemphim;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+public class VideoResponse {
+    @SerializedName("results")
+    private List<Video> results;
+
+    public List<Video> getResults() {
+        return results;
+    }
+}
