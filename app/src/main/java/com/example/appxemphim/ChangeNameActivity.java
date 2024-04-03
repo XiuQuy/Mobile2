ca7d20d648b2fdbd86f05dc48ea9ad64281c9f76
anhvo506 <85537815+anhvo506@users.noreply.github.com>
2024-04-03 22:13:09 +0700

    Update ChangeNameActivity.java

--- app/src/main/java/com/example/appxemphim/ChangeNameActivity.java
+++ app/src/main/java/com/example/appxemphim/ChangeNameActivity.java
@@ -10,8 +10,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.accountinfo.R;
-
 
 public class ChangeNameActivity extends AppCompatActivity {
 
