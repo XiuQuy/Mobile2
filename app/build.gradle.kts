e67c985b1467b40d40c0751369cbc8018187aa9f
Huy Đỗ <100893585+dolehuy00@users.noreply.github.com>
2024-04-12 07:58:57 +0700

    update login fb gg

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.androidApplication)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -38,6 +39,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.legacy.support.v4)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
@@ -52,4 +54,10 @@ dependencies {
     implementation(libs.logging.interceptor)
     implementation("com.pierfrancescosoffritti.androidyoutubeplayer:core:12.1.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.credentials:credentials:1.2.2")
+    implementation("androidx.credentials:credentials-play-services-auth:1.2.2")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.0")
+    implementation("com.facebook.android:facebook-android-sdk:latest.release")
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("com.google.firebase:firebase-auth")
 }
--- app/src/main/java/com/example/appxemphim/data/remote/UserService.java
+++ app/src/main/java/com/example/appxemphim/data/remote/UserService.java
@@ -5,9 +5,17 @@ import com.example.appxemphim.model.User;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface UserService {
-    @GET("api/User/login")
+    @POST("api/User/login")
     Call<User> checkLogin();
+
+    @POST("api/User/login-google")
+    Call<User> checkLoginGoogle(@Body String googleIdToken);
+
+    @POST("api/User/login-facebook")
+    Call<User> checkLoginFacebook(@Body String firebaseAuthenFbIdToken);
 }
