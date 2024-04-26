e2b9ef686d1d2ca0f653bec173380416bc71bf4f
phung <phung@example.com>
2024-04-26 11:50:38 +0700

    playlist,histoty

--- app/src/main/java/com/example/appxemphim/data/remote/HistoryService.java
+++ app/src/main/java/com/example/appxemphim/data/remote/HistoryService.java
@@ -1,10 +1,12 @@
 package com.example.appxemphim.data.remote;
 
+import com.example.appxemphim.model.DeleteResponse;
 import com.example.appxemphim.model.History;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Path;
@@ -21,4 +23,19 @@ public interface HistoryService {
             @Path("userId") int userId,
             @Header("Authorization") String token);
 
+    @GET("api/History/{limit}/{userId}")
+    Call<List<History>> getNewestHistory(
+            @Path("limit") int limit,
+            @Path("userId") int userId,
+            @Header("Authorization") String token);
+
+    @DELETE("api/History/delete-one/{historyId}/{userId}")
+    Call<DeleteResponse> deleteOneHistory(
+            @Path("historyId") int historyId,
+            @Path("userId") int userId,
+            @Header("Authorization") String token);
+
 }
+
+
+
