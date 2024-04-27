caf5a23f2d56bf2a55b8d9d47a3bf0f9d6936574
bonzusa456 <95321447+bonzusa456@users.noreply.github.com>
2024-04-27 22:34:24 +0700

    Create Trailer.java

---
+++ app/src/main/java/com/example/appxemphim/ui/viewmodel/Trailer.java
@@ -0,0 +1,12 @@
+package com.example.appxemphim.ui.viewmodel;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Trailer {
+    @SerializedName("key")
+    private String key;
+
+    public String getKey() {
+        return key;
+    }
+}
