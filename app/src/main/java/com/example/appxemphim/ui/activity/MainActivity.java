f5b5ad8ca8af009dc3b399f80d361237269d2fe9
Huy Đỗ <100893585+dolehuy00@users.noreply.github.com>
2024-04-29 22:06:47 +0700

    update after merge

--- app/src/main/java/com/example/appxemphim/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/appxemphim/ui/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
