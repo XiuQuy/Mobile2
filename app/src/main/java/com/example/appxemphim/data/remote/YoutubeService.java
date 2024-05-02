c250bc0fb03af1a4636107d38d82c057698db8ed
Huy Đỗ <100893585+dolehuy00@users.noreply.github.com>
2024-05-02 19:26:31 +0700

    update

--- app/src/main/java/com/example/appxemphim/data/remote/YoutubeService.java
+++ app/src/main/java/com/example/appxemphim/data/remote/YoutubeService.java
@@ -8,12 +8,12 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface YoutubeService {
-    @GET("videos")
-    Call<YoutubeVideoResponse> getVideoInfo(
-            @Query("part") String[] part,
-            @Query("id") String[] videoId,
-            @Query("key") String apiKey
-    );
+//    @GET("videos")
+//    Call<YoutubeVideoResponse> getVideoInfo(
+//            @Query("part") String[] part,
+//            @Query("id") String[] videoId,
+//            @Query("key") String apiKey
+//    );
 
     @GET("channels")
     Call<YoutubeChannelResponse> getChannelInfo(
