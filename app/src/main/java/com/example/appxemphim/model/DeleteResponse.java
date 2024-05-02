379e9ad574d80500f6c627cc56062f51eaa5039a
KIMPHUNG0510 <mickeyandmini0510@gmail.com>
2024-05-02 22:27:24 +0700

    Playlist, History

--- app/src/main/java/com/example/appxemphim/model/DeleteResponse.java
+++ app/src/main/java/com/example/appxemphim/model/DeleteResponse.java
@@ -2,11 +2,11 @@ package com.example.appxemphim.model;
 
 import com.google.gson.annotations.SerializedName;
 
-public class DeleteResponse {
-    @SerializedName("success")
-    private boolean success;
+public class DeleteResponse {//Lớp DeleteResponse định nghĩa cấu trúc của phản hồi từ hệ thống sau khi gửi một yêu cầu xóa
+    @SerializedName("success")//thuoc tinh success
+    private boolean success;//kieu boolean
 
     public boolean isSuccess() {
         return success;
-    }//xu ly yeu cau phan hoi co thanh cong khong
+    } //kiểm tra giá trị của thuộc tính success để biết xem yêu cầu xóa đã thành công hay không.
 }
