998a88dfd36884c8b44fde74bfea4ca7a45c151a
Huy Đỗ <100893585+dolehuy00@users.noreply.github.com>
2024-05-13 12:21:31 +0700

    Update ServiceApiBuilder.java

--- app/src/main/java/com/example/appxemphim/data/remote/ServiceApiBuilder.java
+++ app/src/main/java/com/example/appxemphim/data/remote/ServiceApiBuilder.java
@@ -35,7 +35,7 @@ public class ServiceApiBuilder {
     // Create OkHttp Client
     private static final OkHttpClient.Builder okHttp =
             new OkHttpClient.Builder()
-                    .readTimeout(50, TimeUnit.SECONDS)
+                    .readTimeout(500, TimeUnit.SECONDS)
                     .addInterceptor(new Interceptor() {
                         @Override
                         public Response intercept(Chain chain) throws IOException {
